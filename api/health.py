9eb865af2bb9871437aa79fe410b3c121ccc70f9
feiju12138 <49089085+feiju12138@users.noreply.github.com>
2026-02-28 11:19:14 +0800

    Delete api/health.py

--- api/health.py
+++
@@ -1,9 +0,0 @@
-def handler(request):
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "text/plain",
-            "Access-Control-Allow-Origin": "*"
-        },
-        "body": "ok"
-    }
